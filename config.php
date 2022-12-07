f2051a688301945de7e9e8f5230c4582902923d2
wolfgang101 <me@wolfgang.lol>
2022-12-07 17:05:07 +0100

    fix: removed custom config.php from repository

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    define('INCLUDE_MASTODON_FEED_DEBUG', true);
-    
-    // define('INCLUDE_MASTODON_FEED_DEFAULT_INSTANCE', 'social.fsoc.lol');
-    // define('INCLUDE_MASTODON_FEED_TEXT_LOADING', 'LLLL Loading');
-    // define('INCLUDE_MASTODON_FEED_TEXT_BOOSTED', 'BBBB..oosted');
-    // define('INCLUDE_MASTODON_FEED_TEXT_VIEW_ON_INSTANCE', 'view elsewhere');
-    // define('INCLUDE_MASTODON_FEED_TEXT_SHOW_CONTENT', 'Show contents');
