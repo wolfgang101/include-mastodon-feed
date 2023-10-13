41003549d576cae7ffdcbfcd0451dd3485126667
wolfgang101 <me@wolfgang.lol>
2023-10-13 13:18:26 +0200

    docs: updated version number and changelog

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
   Plugin Name: Include Mastodon Feed
 	Plugin URI: https://wolfgang.lol/code/include-mastodon-feed-wordpress-plugin
 	Description: Plugin providing [include-mastodon-feed] shortcode
-	Version: 1.9.2
+	Version: 1.9.3
 	Author: wolfgang.lol
 	Author URI: https://wolfgang.lol
 */
