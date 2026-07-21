d23090b701dedc655fa16b55bcd42f8f9f60c074
wolfgang101 <me@wolfgang.lol>
2026-07-22 01:03:29 +0200

    fix: pattern did not store "limit" correctly

--- plugin/gutenberg/build/index.asset.php
+++ plugin/gutenberg/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ff9e6559faa1c29f92b3');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '09b665284f5ec41002d6');
